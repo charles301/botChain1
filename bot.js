d687b7166946e7f3f8eff5dd98dced1fe6404ab4
Charles Wilson <charlesw124@gmail.com>
2021-12-06 17:54:39 +0000

    push for steve

--- bot.js
+++ bot.js
@@ -1,27 +1,19 @@
 const Discord = require("discord.js");
-const SHA256 = require('crypto-js/sha256')
+const axios = require("axios")
+const dotenv = require("dotenv")
+
 
 const config = require("./config.json");
-const Blockchain = require("./blockchain");
 const client = new Discord.Client();
 
+const result = dotenv.config()
+console.log(result)
+const token = process.env.BEARER_TOKEN
+console.log(token)
 
-let disc0in = new Blockchain();
-incrementor = 1
+const url = "http://api.coincap.io/v2/assets/amp"
 
-class Block{
-  constructor(index, timestamp, data, previousHash = ""){
-      this.index = index
-      this.timestamp = timestamp
-      this.data = data
-      this.previousHash = previousHash
-      this.hash = this.calculateHash()
 
-  }
-  calculateHash(){
-      return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString()
-  }
-}
 
 const prefix = "#";
 client.on("message", function(message) {
@@ -36,72 +28,30 @@ client.on("message", function(message) {
   let user = message.author.id;
   let content = message.content;
   
-  
-  
-  disc0in.addBlock(new Block(incrementor, Date, {amount: 1, "user" : user,"content": content}))
+  if (command === "price") {
+    let AMP 
 
-  incrementor++
-  if (command === "dc") {
-   let valid = disc0in.isChainValid();
-   let latest = disc0in.getLatestBlock();
-   let chain = disc0in;
-   let previousContent = disc0in
-   message.reply(valid);
-   console.log(disc0in)
-   console.log(previousContent)
+function getRequest() {
+  axios.get(url, {
+      headers: { Authorization: 'Bearer ' + token }
+  }
+  )
+      .then( function (response) {
+          console.log(response.data.data.priceUsd)
+          AMP = response.data.data.priceUsd 
+          message.reply(`AMP USD price is :${AMP}`)
+      })
+      .catch(function (error) {
+          console.log(error)
+          currentPrices.push("error");
+      })
+}
+    getRequest()
+    console.log(AMP)
+     
+    
   }
 
-//   if (!message.content.startsWith(prefix)) return;
-
-//   const commandBody = message.content.slice(prefix.length);
-//   const args = commandBody.split(' ');
-//   const command = args.shift().toLowerCase();
-
-
-
-//   else if (command === "sum") {
-//     const numArgs = args.map(x => parseFloat(x));
-//     const sum = numArgs.reduce((counter, x) => counter += x);
-//     message.reply(`The sum of all the arguments you provided is ${sum}!`);
-//   }
-//   else if (command === "shaun") {
-//       for(i=0; i < 100; i++){
-//         message.s
-//       }
-//   }
-// meesin: 90779765207736320
-// const steve = ["So how does that relate to code?","Oh yeah, interesting, have you been coding today?","Have you made hangman yet?","So thats your excuse for not coding then?", "Go and have a coffee and start coding","Open youtube and watch a tutorial on how to makea discord bot"]
-//   if (message.author.id == '258631353518653440'){
-//      let rand = Math.floor(Math.random() * 6)
-//     message.channel.send(`${steve[rand]}`)
-//   .then(message => console.log(`Sent message: ${message.content}`))
-//   .catch(console.error);
-//   }
-
-//   if (message.author.id == '90779765207736320'){
-//     //let rand = Math.floor(Math.random() * 6)
-//    message.reply("uWu")
-//    message.reply(message.content)
-//  .then(message => console.log(`Sent message: ${message.content}`))
-//  .catch(console.error);
-//  }
-    //if (message.author.id == '141963410467323904')
-    //message.channel.send('no.')
-//   console.log(message.author.id)
-//   console.log(message.author)
-//   const commandBody = message.content
-//   console.log(commandBody)
-
-//     //if (message.author.id == '90779765207736320')
-//  // message.channel.send('no.')
-// //console.log(message.author.id)
-// let numb = Math.random(Math.floor()*10)
-//     if (message.author.id == '831611386525515817'){
-
-//     if (numb == 1){
-//     message.channel.send('Daddy senpai uWu <3')
-//     }
-//   }
 });
 
 client.login(config.BOT_TOKEN);
